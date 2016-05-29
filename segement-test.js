de8372e4822993758d51f0597fd8c1efb2f724f1
Geoff <geoff616@gmail.com>
2016-05-29 04:23:39 +0200

    cleanup test

--- segement-test.js
+++ segement-test.js
@@ -3,16 +3,19 @@ var path = require('path');
 var Analytics = require('analytics-node');
 var analytics = new Analytics('dhzziat62bLVkuC1oT0NipPuDbe0LtYJ');
 
-fs.readFile(path.join(__dirname, 'fakeData/3-OrdercreateRS.xml'), 'utf8', function(err, rs) {
+fs.readFile(path.join(__dirname, 'fakeData/3-OrdercreateRS.xml'), 'utf8', function(err, RS) {
   if (err) throw err;
-  fs.readFile(path.join(__dirname, 'fakeData/3-OrdercreateRQ.xml'), 'utf8', function(err, rq) {
+  fs.readFile(path.join(__dirname, 'fakeData/3-OrdercreateRQ.xml'), 'utf8', function(err, RQ) {
     if (err) throw err;
     analytics.track({
       anonymousId: 2345,
-      event: 'stuff2',
+      event: 'transaction',
       properties: {
-        NDC_request: 'rand' + rs,
-        NDC_response: rq
+        _meta: {
+          // arbitrary key value pairs 
+        },
+        NDC_request: RQ,
+        NDC_response: RS
       }
     });
     console.log('all done!')
